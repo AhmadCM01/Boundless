7e2700c5fdd4429614bd46147bbd42c81732362a
Ahmad Umar <ahmadyeruwa2008@gmail.com>
2026-07-25 21:42:50 +0100

    Fix missing useEffect import in App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { RoomProvider } from './context/RoomContext';
 import { Navbar } from './components/Navbar';
 import { Toolbar, ToolMode } from './components/Toolbar';
